f52dd81edf946f7020267163c5c3e75d64757ed6
Terts <terts.diepraam@gmail.com>
2016-08-28 12:59:11 +0200

    Commented out bluetooth

--- PiWatch/apps/bluetooth_app.py
+++ PiWatch/apps/bluetooth_app.py
@@ -1,68 +1,68 @@
-import bluetooth
-from pi_utils import *
-
-
-def define_services():
-    service = Service(
-        name='bluetooth service'
-    )
-
-    @service.event_listener('boot')
-    def boot_bluetooth(event):
-        pass
-
-    @service.event_listener('bl discover')
-    @threaded
-    def discover_devices(event):
-        print('Discovering Devices')
-        try:
-            nearby_devices = bluetooth.discover_devices()
-        except OSError:
-            print('No devices found')
-            service.global_eventqueue.add(Event('bl no devices found'))
-        else:
-            return_value = []
-            for bdaddr in nearby_devices:
-                return_value.append(bluetooth.lookup_name(bdaddr))
-                print('Done with Discovering')
-                service.global_eventqueue.add(Event('bl discovered', data=return_value))
-
-    return service
-
-
-def define_app():
-    app = App(
-        name='bluetooth app'
-    )
-    main = Activity(
-        name='main'
-    )
-
-    discover_bttn = Text(
-        message='Discover Devices',
-        size=30,
-        position=('midtop', 0, 10),
-        bg_color=(50, 50, 50),
-    )
-
-    discovered_devices = List(
-        position=('midtop', 0, 50),
-    )
-    discovered_devices.add(Text(message='No Discovered Devices'))
-
-    @main.event_listener('mouse_down')
-    def mouse_down_handler(event):
-        if discover_bttn.check_collision(event.pos):
-            app.global_eventqueue.add(Event('bl discover'))
-
-    @app.event_listener('bl discovered')
-    def bl_discovered(event):
-        adapter = [str_to_text(string) for string in event.data]
-        discovered_devices.clear()
-        discovered_devices.add(*adapter)
-
-
-    main.add(discover_bttn, discovered_devices)
-    app.add(main)
-
-    return app
+# import bluetooth
+# from pi_utils import *
+#
+#
+# def define_services():
+#     service = Service(
+#         name='bluetooth service'
+#     )
+#
+#     @service.event_listener('boot')
+#     def boot_bluetooth(event):
+#         pass
+#
+#     @service.event_listener('bl discover')
+#     @threaded
+#     def discover_devices(event):
+#         print('Discovering Devices')
+#         try:
+#             nearby_devices = bluetooth.discover_devices()
+#         except OSError:
+#             print('No devices found')
+#             service.global_eventqueue.add(Event('bl no devices found'))
+#         else:
+#             return_value = []
+#             for bdaddr in nearby_devices:
+#                 return_value.append(bluetooth.lookup_name(bdaddr))
+#                 print('Done with Discovering')
+#                 service.global_eventqueue.add(Event('bl discovered', data=return_value))
+#
+#     return service
+#
+#
+# def define_app():
+#     app = App(
+#         name='bluetooth app'
+#     )
+#     main = Activity(
+#         name='main'
+#     )
+#
+#     discover_bttn = Text(
+#         message='Discover Devices',
+#         size=30,
+#         position=('midtop', 0, 10),
+#         bg_color=(50, 50, 50),
+#     )
+#
+#     discovered_devices = List(
+#         position=('midtop', 0, 50),
+#     )
+#     discovered_devices.add(Text(message='No Discovered Devices'))
+#
+#     @main.event_listener('mouse_down')
+#     def mouse_down_handler(event):
+#         if discover_bttn.check_collision(event.pos):
+#             app.global_eventqueue.add(Event('bl discover'))
+#
+#     @app.event_listener('bl discovered')
+#     def bl_discovered(event):
+#         adapter = [str_to_text(string) for string in event.data]
+#         discovered_devices.clear()
+#         discovered_devices.add(*adapter)
+#
+#
+#     main.add(discover_bttn, discovered_devices)
+#     app.add(main)
+#
+#     return app
