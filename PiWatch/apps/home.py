69e9e8d8d69c3764c7bfcb7b0997de5dcd204760
Terts <terts.diepraam@gmail.com>
2017-01-11 00:12:06 +0100

    Added a word clock!

--- PiWatch/apps/home.py
+++ PiWatch/apps/home.py
@@ -1,4 +1,5 @@
 from piwatch import *
+import time
 
 appsfolder = 'apps'
 
@@ -6,7 +7,120 @@ NORMAL = 0
 HEX = 1
 BIN = 2
 NOW = 3
-num_states = 4
+TEXT = 4
+num_states = 5
+
+
+def hexify(int):
+    return str(hex(int))[2:].upper()
+
+
+def text_time(time):
+    def int_to_word(int):
+        if int == 0:
+            return 'zero'
+        elif int == 1:
+            return 'one'
+        elif int == 2:
+            return 'two'
+        elif int == 3:
+            return 'three'
+        elif int == 4:
+            return 'four'
+        elif int == 5:
+            return 'five'
+        elif int == 6:
+            return 'six'
+        elif int == 7:
+            return 'seven'
+        elif int == 8:
+            return 'eight'
+        elif int == 9:
+            return 'nine'
+        elif int == 10:
+            return 'ten'
+        elif int == 11:
+            return 'eleven'
+        elif int == 12:
+            return 'twelve'
+        elif int == 13:
+            return 'thirteen'
+        elif int == 14:
+            return 'fourteen'
+        elif int == 15:
+            return 'fifteen'
+        elif int == 18:
+            return 'eighteen'  # just one "t"
+        # 16 - 19 except 18
+        elif int < 20:
+            return int_to_word(int - 10) + 'teen'
+        # 20 - 29
+        elif int == 20:
+            return 'twenty'
+        elif int < 30:
+            return 'twenty-' + int_to_word(int - 20)
+        # 30 - 39
+        elif int == 30:
+            return 'thirty'
+        elif int < 40:
+            return 'thirty-' + int_to_word(int - 30)
+        # 40 - 49
+        elif int == 40:
+            return 'forty'
+        elif int < 50:
+            return 'forty-' + int_to_word(int - 40)
+        # 50 - 59
+        elif int == 40:
+            return 'fifty'
+        elif int < 60:
+            return 'fifty-' + int_to_word(int - 50)
+        # 60
+        elif int == 60:
+            return 'sixty'
+
+    time = time[3:5]
+    hour = time[0]
+    if hour == 12:
+        next_hour = 1
+    elif hour > 12:
+        hour %= 12
+        next_hour = hour + 1
+    else:
+        next_hour = hour + 1
+    minutes = time[1]
+
+    if hour == 0:
+        if minutes == 0:
+            return 'midnight'
+        else:
+            hour_str = 'midnight'
+    elif hour == 12:
+        if minutes == 0:
+            return 'noon'
+        else:
+            hour_str = 'noon'
+    else:
+        hour_str = int_to_word(hour)
+
+    if next_hour == 0:
+        next_hour_str = 'midnight'
+    elif hour == 12:
+        next_hour_str = 'noon'
+    else:
+        next_hour_str = int_to_word(hour)
+
+    if minutes == 0:
+        return hour_str + " o'clock"
+    elif minutes == 30:
+        return 'half past ' + hour_str
+    elif minutes == 15:
+        return 'quarter past ' + hour_str
+    elif minutes == 45:
+        return 'quarter to ' + next_hour_str
+    elif minutes > 30:
+        return int_to_word(60 - minutes) + ' to ' + next_hour_str
+    else:
+        return int_to_word(minutes) + ' past ' + hour_str
 
 
 class FunClock(Text):
@@ -28,13 +142,13 @@ class FunClock(Text):
 
     def hex_message(self):
         self.time = time.localtime()[3:5]
-        hours = str(bin(self.time[0])) if self.twentyfour else str(hex(self.time[0] % 12))[2:]
-        minutes = str(hex(self.time[1]))[2:] if len(str(hex(self.time[1]))) > 1 else '0' + str(hex(self.time[1]))[2:]
+        hours = hexify(self.time[0]) if self.twentyfour else hexify(self.time[0] % 12)
+        minutes = hexify(self.time[1]) if len(hexify(self.time[1])) > 1 else '0' + hexify(self.time[1])
         return hours + self.separator + minutes
 
     def bin_message(self):
         self.time = time.localtime()[3:5]
-        hours = str(bin(self.time[0])) if self.twentyfour else str(bin(self.time[0] % 12))[2:]
+        hours = str(bin(self.time[0]))[2:] if self.twentyfour else str(bin(self.time[0] % 12))[2:]
         minutes = str(bin(self.time[1]))[2:]
         return hours + self.separator + minutes
 
@@ -49,6 +163,12 @@ class FunClock(Text):
             elif kwargs['state'] == BIN:
                 kwargs['message'] = self.bin_message()
                 kwargs['size'] = 40
+            elif kwargs['state'] == NOW:
+                kwargs['message'] = 'now'
+                kwargs['size'] = 120
+            elif kwargs['state'] == TEXT:
+                kwargs['message'] = text_time(time.localtime())
+                kwargs['size'] = 27
         super().update(**kwargs)
 
     def draw(self, surface):
@@ -61,9 +181,9 @@ class FunClock(Text):
         elif self.state == BIN:
             if self.time != time.localtime()[3:5]:
                 self.update(message=self.bin_message())
-        elif self.state == NOW:
-            if not self.message == "now":
-                self.update(message="now")
+        elif self.state == TEXT:
+            if self.time != time.localtime()[3:5]:
+                self.update(message=text_time(time.localtime()))
         super().draw(surface)
 
 
@@ -77,13 +197,14 @@ def define_app():
     # Components
     mainclock = FunClock(
         size=96,
-        position=('center', 0, -25),
+        position=('midbottom', 0, -110),
         separator=':'
     )
 
     maindate = Date(
-        position=('center', 0, 30),
-        size=25
+        position=('center', 0, 40),
+        size=25,
+        color=(200, 200, 200)
     )
 
     @app.event_listener('mouse up')
